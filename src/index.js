2bbc2efc50335578dc5823ba208d8b456fb9b655
Izak Filmalter <izak@whoatech.com>
2017-01-03 17:09:02 +0200

    Delete stuff. Add react router. Gitnor web storm.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Styletron from 'styletron-client';
+import { StyletronProvider } from 'styletron-react';
 import './index.css';
 
+const styleElements = document.getElementsByClassName('_styletron_hydrate_');
+
 ReactDOM.render(
-  <App />,
+  <StyletronProvider styletron={new Styletron(styleElements)}>
+    <App />
+  </StyletronProvider>,
   document.getElementById('root')
 );
---
+++ src/styles.js
@@ -0,0 +1,7 @@
+import { styled } from 'styletron-react';
+
+export const AppContainer = styled('div', {
+  background: 'red',
+  height: '100vh',
+  width: '100vw',
+});
