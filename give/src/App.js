c8b89d89d872132dbec34cb46f8c87d8563c8cbe
Izak Filmalter <izak@whoatech.com>
2017-02-14 13:35:44 -0500

    More marketing work.

--- give/src/App.js
+++ give/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import Button from 'react-toolbox/lib/button/Button'
+
 import './App.css';
 
 class App extends Component {
@@ -10,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Button raised primary>yolo</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- give/src/index.js
+++ give/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import './index.css';
 
+import theme from '../public/react-toolbox/theme.js'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import '../public/react-toolbox/theme.css'
+
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
