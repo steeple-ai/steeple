66e5d0f7ffd7c4da0ef0c84830d9625eb2e76f09
Izak Filmalter <izak@whoatech.com>
2017-03-13 13:16:32 -0400

    Minify logotype and logo svg

--- marketing/src/components/logo/Logo.jsx
+++ marketing/src/components/logo/Logo.jsx
@@ -7,22 +7,75 @@ import {
 class Logo extends Component {
   render() {
     return (
-      <LogoContainer width="32px" height="32px" viewBox="24 8 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" {...this.props}>
+      <LogoContainer
+        width="32"
+        height="32"
+        viewBox="24 8 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        {...this.props}
+      >
         <defs>
-          <circle id="path-1" cx="16" cy="16" r="16" style={{ overflow: 'hidden' }} />
-          <linearGradient x1="87.8155791%" y1="0%" x2="44.7136346%" y2="46.626616%" id="linearGradient-3">
-            <stop stopColor="#FFC107" offset="0%" />
-            <stop stopColor="#FFA000" offset="100%" />
+          <circle
+            id="a"
+            cx="16"
+            cy="16"
+            r="16"
+            overflow="hidden"
+          />
+          <linearGradient
+            x1="87.816%"
+            y1="0%"
+            x2="44.714%"
+            y2="46.627%"
+            id="b"
+          >
+            <stop
+              stopColor="#FFC107"
+              offset="0%"
+            />
+            <stop
+              stopColor="#FFA000"
+              offset="100%"
+            />
           </linearGradient>
         </defs>
-        <g id="Group-3" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(24.000000, 8.000000)">
-          <mask id="mask-2" fill="white">
-            <use xlinkHref="#path-1" />
+
+        <g
+          fill="none"
+          fillRule="evenodd"
+          transform="translate(24 8)"
+        >
+          <mask
+            id="c"
+            fill="#fff"
+          >
+            <use
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+               xlinkHref="#a"
+            />
           </mask>
-          <use id="Oval" fill="#FFC107" xlinkHref="#path-1" />
-          <polygon id="Rectangle-2" fill="url(#linearGradient-3)" mask="url(#mask-2)" transform="translate(17.281039, 22.399246) rotate(45.000000) translate(-17.281039, -22.399246) " points="5.06243439 11.9914997 29.4996429 11.9914997 25.877926 32.8069914" />
-          <polygon id="Triangle" fill="#3F51B5" mask="url(#mask-2)" points="16 6.4 28.8 38.4 16 38.4" />
-          <polygon id="Triangle-Copy" fill="#7986CB" mask="url(#mask-2)" points="16 6.4 3.2 38.4 16 38.4" />
+          <use
+            fill="#FFC107"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            xlinkHref="#a"
+          />
+          <path
+            fill="url(#b)"
+            mask="url(#c)"
+            transform="rotate(45 17.28 22.4)"
+            d="M5.062 11.99H29.5l-3.622 20.817"
+          />
+          <path
+            fill="#3F51B5"
+            mask="url(#c)"
+            d="M16 6.4l12.8 32H16"
+          />
+          <path
+            fill="#7986CB"
+            mask="url(#c)"
+            d="M16 6.4l-12.8 32H16"
+          />
         </g>
       </LogoContainer>
     );
